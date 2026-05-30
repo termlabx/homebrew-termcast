4bb27efffac2c593b0e4e2395a85f554de6ba761
ulixcodeLab-dev <87634290+ulixcode-labs@users.noreply.github.com>
2026-05-30 13:00:57 -0700

    chore: bump Termcast to 0.15.0

--- Casks/termcast.rb
+++ Casks/termcast.rb
@@ -1,6 +1,6 @@
 cask "termcast" do
-  version "0.12.0"
-  sha256 "1c0fa69d559c6fde2be8405979fa44730b57b6e2fe3d1c7f8fed9bf1f2ecbec8"
+  version "0.15.0"
+  sha256 "370fcb94a1bebd00f9f9643cfe292d1625a97c6a59f01f53667a7a9101eca4e3"
 
   url "https://github.com/termlabx/termcast_server/releases/download/v#{version}/termcast_server.dmg"
   name "Termcast"
