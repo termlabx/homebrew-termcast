9b4baebf34424e622de8bb9dd3ae90582dfe9dfb
ulixcodeLab-dev <87634290+ulixcode-labs@users.noreply.github.com>
2026-06-26 10:15:20 -0700

    chore: bump Termcast to 0.109.0

--- Casks/termcast.rb
+++ Casks/termcast.rb
@@ -1,6 +1,6 @@
 cask "termcast" do
-  version "0.107.0"
-  sha256 "31629b748c70066ca41330dd878dc20a57d68f9e28d84a849003421fa49ff821"
+  version "0.109.0"
+  sha256 "9b4c76a20154d64c739813258103b80e02e03540c14bc42ace2b996cb5609f32"
 
   url "https://relay.example.com/releases/termcast.dmg?v=#{version}"
   name "Termcast"
