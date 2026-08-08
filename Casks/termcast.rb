792bc0ccad7297d8c624a53a197b0db0f470f4e6
ulixcodeLab-dev <87634290+ulixcode-labs@users.noreply.github.com>
2026-08-08 15:45:37 -0700

    chore: bump Termcast to 0.177.0

--- Casks/termcast.rb
+++ Casks/termcast.rb
@@ -1,6 +1,6 @@
 cask "termcast" do
-  version "0.176.0"
-  sha256 "d1ddcfc510968ee452221eace71282a76509346e2ce85c130a8ea864298445b8"
+  version "0.177.0"
+  sha256 "7827d27782ef9ecd9069f1a79d8454fe87bbd3a8426f1a894d37c2aefa0b80c9"
 
   url "https://ttyd-relay.xing-mathcoder.workers.dev/releases/termcast.dmg?v=#{version}"
   name "Termcast"
